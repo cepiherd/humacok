8d0e1b1cc74bd390915a2528e89897b570969d29
cepiherd <cepiherd@gmail.com>
2024-09-25 09:43:47 +0700

    update

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -8,18 +8,18 @@ export const metadata: Metadata = {
 export default function Page() {
   const [title,setTitle] = useState('')
   const [kejadian , setKejadian ] = useState('')
-  const subitHandle =(data) => {
+  const subitHandle =(data : {title : string , kejadian : string}) => {
     window.localStorage.setItem('form-kejadian' , JSON.stringify(data))
   }
   return (
     <>
       <h1>This is offline fallback page</h1>
       <h2>When offline, any page route will fallback to this page</h2>
-      <input type="text" value={title} onInput={(e) => setTitle(e.target.value)} />
+      <input type="text" value={title} onInput={(e : any) => setTitle(e.target.value)} />
       <label htmlFor="">
         Lokasi
       </label>
-      <input type="text" value={kejadian} onInput={(e) => setKejadian(e.target.value)} />
+      <input type="text" value={kejadian} onInput={(e : any) => setKejadian(e.target.value)} />
       <button onClick={()=> subitHandle({title , kejadian})}></button>
     </>
   );
